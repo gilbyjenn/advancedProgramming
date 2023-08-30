9fac1865d0d7afc3caa1376fa0c923ef0607fcf1
jennifergilby <jennifergilby@10.131.222.82>
2023-08-30 14:50:01 -0400

    Initial commit (create file on Eclipse and load into repository)

---
+++ src/lab1/Lab1.java
@@ -0,0 +1,13 @@
+package lab1;
+
+public class Lab1 {
+	
+	/*
+	 * (1) Write code to print to the console 1,000 randomly generated 
+		DNA 3 mers (e.g. “ACA”, “TCG” )
+		where the frequency of A,C,G and T is 25% and is uniformly sampled.
+	*/ 
+
+		
+
+}
