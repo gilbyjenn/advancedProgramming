26265948e875f4a4f7c1f1b73c0a4d048525728b
jennifergilby <jennifergilby@10.131.222.82>
2023-09-01 14:46:38 -0400

    3mer generation with adjusted probabilities and AAA count

--- src/lab1/Lab1.java
+++ src/lab1/Lab1.java
@@ -3,20 +3,21 @@ package lab1;
 import java.util.Random;
 
 public class Lab1 {
-	
 
 	public static void main(String[] args)
 		{ 
 		Random random = new Random();
 		
-		int aaa = 0;
+		int aaa_frequency = 0;
 		
 		for( int x=0; x < 1000; ) 
 			{ 
 			String s = "";
 			for (int y=0; y<3; y++)
+				
 				{
 				int n = random.nextInt(4);
+				
 				if (n == 0)
 					s += "A";
 				else if (n == 1)
@@ -26,18 +27,42 @@ public class Lab1 {
 				else if (n == 3)
 					s += "T";
 				}
+				
 				if (s.equals("AAA"))
-					aaa++;
-			//System.out.println(s);
-		
-			x++; // increments x to reset the loop 
+					aaa_frequency++;
+	
+			x++; 
 			}
-		System.out.println("3mer AAA was generated " + aaa + " times.");
-			
+		System.out.println("3mer AAA was generated " + aaa_frequency + " times with equal probabilities.");
+		
+		// Modify code to adjust frequency of A,C,G and T:  
+		
+		int aaa_modified = 0;
+		
+		for( int x=0; x < 1000; ) 
+			{ 
+			String s = "";
+			for (int y=0; y<3; y++)
+				
+				{
+				int n = random.nextInt(101);
+				
+				if (n>= 0 && n <= 11)
+					s += "A";
+				else if (n>= 12 && n <= 49)
+					s += "C";
+				else if (n>= 50 && n <= 89)
+					s += "G";
+				else if (n>= 90 && n <= 100)
+					s += "T";
+				}
+				
+				if (s.equals("AAA"))
+					aaa_modified++;
+		
+			x++; 
+			}
+		System.out.println("3mer AAA was generated " + aaa_modified + " times after probabilities were adjusted.");	
 		}
-
 	
 }
-
-
-// can just count how many of each 3mer there is rather than storing or printing all of them
